c346505619d130c9a1cec182a155aae931debf7c
alexbidenko1998 <dka@mstagency.ru>
2020-12-19 12:30:35 +0600

    cities fix

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -25,24 +25,18 @@
             <span class="Popup__label">Название компании: </span>
             <span class="Popup__value">{{ info.name }}</span>
           </div>
-          <span class="Popup__label">Города в которых мы находимся: </span>
-          <div class="Popup__grid Popup__space" >
-            <div class="Popup__grid-item" v-for="item in info.cities" :key="item">
-              <span class="Popup__value">{{ item }}</span>
-            </div>
-          </div>
           <span class="Popup__label">Информация о продуктах: </span>
           <div class="Popup__space">
             <div>
-              <span class="Popup__label">Всего продуктов: </span>
+              <span class="Popup__label">Общее количество товаров: </span>
               <span class="Popup__value">{{ info.status.totalProducts }}</span>
             </div>
             <div>
-              <span class="Popup__label">Доступно продуктов: </span>
+              <span class="Popup__label">Товар в наличии: </span>
               <span class="Popup__value">{{ info.status.availableProducts }}</span>
             </div>
             <div>
-              <span class="Popup__label">Недоступно продуктов: </span>
+              <span class="Popup__label">Товар без наличия: </span>
               <span class="Popup__value">{{ info.status.nonAvailableProducts }}</span>
             </div>
           </div>
@@ -50,6 +44,23 @@
             <span class="Popup__label">Последний сеанс: </span>
               <span class="Popup__value">{{ info.status.lastCheckDate }}</span>
           </div>
+          <span class="Popup__label">Города в которых мы находимся: </span>
+            <v-container fluid class="Popup__nonpadding">
+              <v-row align="center">
+                <v-col>
+                  <v-select
+                    v-model="select"
+                    :items="items"
+                    item-text="state"
+                    item-value="abbr"
+                    label="Выберите город"
+                    persistent-hint
+                    return-object
+                    single-line
+                  ></v-select>
+                </v-col>
+              </v-row>
+          </v-container>
         </v-card-text>
 
         <v-divider></v-divider>
@@ -76,7 +87,7 @@
         .then(response => response.json())
         .then(json => {
           this.info.name = json.name 
-          this.info.cities = json.cities
+          this.items = json.cities
           this.info.status.totalProducts = json.status.totalProducts
           this.info.status.availableProducts = json.status.availableProducts
           this.info.status.nonAvailableProducts = json.status.nonAvailableProducts
@@ -85,6 +96,9 @@
     },
     data () {
       return {
+        select: { state: '' },
+        items: [
+        ],
         dialog: false,
         info: {
             name: '',
@@ -107,6 +121,10 @@
       &__value, &__label {
         font-weight: bold;
         font-size: 16px;
+      }
+
+      &__nonpadding {
+        padding: 0 !important;
       }
       
       &__label {
